0ec24b49af9a5bb22a8e5c47a80e3420d3ca34f0
goseungduk <a457581@catholic.ac.kr>
2022-06-24 05:09:01 +0900

    fix: _document typing change

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,14 +1,11 @@
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext,
-} from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
+import { DocumentContext, DocumentInitialProps } from "next/document";
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
     try {
@@ -21,12 +18,6 @@ class MyDocument extends Document {
       const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
       };
     } finally {
       sheet.seal();
